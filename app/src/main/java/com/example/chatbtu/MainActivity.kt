43c1d3e3387ac5ba721a25557f1caaed9895e131
student <student@BTU.local>
2024-10-30 11:00:00 +0400

    bottom navigation

--- app/src/main/java/com/example/chatbtu/MainActivity.kt
+++ app/src/main/java/com/example/chatbtu/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.chatbtu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var bottomNavigationView : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_menu)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                    as NavHostFragment
+
+        val controller = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.messageFragment,
+                R.id.profileFragment
+            )
+        )
+        setupActionBarWithNavController(controller, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(controller)
+
     }
 }
